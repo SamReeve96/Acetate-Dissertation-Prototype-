880ff924a341feff382a867d3f4d5089e57fa703
Samuel Reeve <sreeve96@gmail.com>
2019-12-09 18:18:30 +0000

    Move demo page out of extension folder
    Also properly initialize container if auto load is on

--- extension/contentScripts/container/container.js
+++ extension/contentScripts/container/container.js
@@ -5,7 +5,40 @@ chrome.storage.sync.get('activeOnPageLoad', function (data) {
     let savedStateActive = data.activeOnPageLoad;
 
     if (savedStateActive) {
-        document.body.innerHTML = '<div id="containedBody">' + document.body.innerHTML + '</div>';
+        // need to remove this on close
+        document.head.innerHTML = document.head.innerHTML + 
+        '<script>' +
+            'function fireAddAnno() {' +
+                'var evt = document.createEvent("Event");' +
+                'evt.initEvent("myCustomEvent", true, false);' +
+                
+                '// fire the event' +
+                'document.dispatchEvent(evt); ' +
+            '} +'
+        '</script>'
+
+        document.body.innerHTML = 
+        '<div id="containedBody">' + document.body.innerHTML + '</div>' +
+
+        '<commentsContainer>' +
+            '<h1>Annotations</h1>' +
+        '</commentsContainer>' +
+
+        '<template id="commentBox ">' +
+            '<div class="commentBoxContainer">' +
+                '<textarea rows="4" cols="50"> '+
+                    'add comment here' +
+                '</textarea> '+
+                '<button onclick="fireAddAnno()">Add annotation</button'
+            '</div>' +
+        '</template>';
+
+        let commentsContainerElem = document.querySelector('commentsContainer');
+        let commentBoxTemplate =  document.querySelector('template');
+
+        //Create new comment instance
+        let clone = document.importNode(commentBoxTemplate.content, true);
+        commentsContainerElem.appendChild(clone);
     }
     //If the container isnt active in settings dont wrap content
 
