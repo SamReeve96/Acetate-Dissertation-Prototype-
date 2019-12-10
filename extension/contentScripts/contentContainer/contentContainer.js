a738661ae71818259b19b04e0e1378f98a5e8030
Samuel Reeve <sreeve96@googlemail.com>
2019-12-10 13:23:28 +0000

    File renames for content container script (#1)

---
+++ extension/contentScripts/contentContainer/contentContainer.js
@@ -0,0 +1,14 @@
+// Code to be injected into the content page
+function fireAddnno() {
+    var evt = document.createEvent("Event");
+    evt.initEvent("myCustomEvent", true, false);
+
+    // fire the event
+    document.dispatchEvent(evt);
+}
+
+function test() {
+    alert('Hello world!');
+}
+
+alert('Hello Sam!');
