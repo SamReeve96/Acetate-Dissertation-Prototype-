7b3eaec985ff3e2843a758c7ab4979f89885b745
Samuel Reeve <sreeve96@googlemail.com>
2020-01-30 22:20:10 +0000

    Add popup lint fixes (#47)

--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -1,22 +1,22 @@
-let activateButton = document.getElementById('Activate');
+const activateButton = document.getElementById('Activate');
 
-activateButton.onclick = function () {
-    let message = {
+activateButton.onclick = function() {
+    const message = {
         type: 'changeActiveState'
     };
 
     chrome.runtime.sendMessage(message);
 };
 
-let darkModeButton = document.getElementById('DarkMode');
-darkModeButton.addEventListener('click', function() {
-        let message = {
+const darkModeButton = document.getElementById('DarkMode');
+darkModeButton.addEventListener('click', () => {
+    const message = {
         type: 'changeTheme'
     };
 
-    //Send message
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, message, function () {
+    // Send message
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        chrome.tabs.sendMessage(tabs[0].id, message, () => {
             console.log('message sent');
         });
     });
