1ef647f642bc3d091f0b9faab2782ad3506cc44f
Samuel Reeve <sreeve96@googlemail.com>
2020-02-10 23:19:16 +0000

    Remove generated es-lint json for firebase (#13)

--- server/functions/index.js
+++ server/functions/index.js
@@ -1,8 +1,9 @@
 const functions = require('firebase-functions');
 
 exports.bigben = functions.https.onRequest((req, res) => {
-  const hours = (new Date().getHours() % 12) + 1  // London is UTC + 1hr;
-  res.status(200).send(`<!doctype html>
+    // London is UTC + 1hr;
+    const hours = (new Date().getHours() % 12) + 1;
+    res.status(200).send(`<!doctype html>
     <head>
       <title>Time</title>
     </head>
